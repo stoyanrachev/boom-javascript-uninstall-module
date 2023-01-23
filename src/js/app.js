f7eeacbc5c029631cb136fadb794189f8a67d0d1
DESKTOP-IN4R64E\strac <strachev@gmail.com>
2023-01-23 20:26:17 +0200

    remove Ramda package

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,17 @@
 import "../scss/app.scss";
-const R = require("ramda");
+//const R = require("ramda");
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
   const ul = document.querySelector("ul");
   const numbersArray = [1, 3, 4, 5, 6, 7];
-  const isEven = (n) => n % 2 === 0;
-  const filteredArray = R.filter(isEven, numbersArray);
+  //const isEven = (n) => n % 2 === 0;
+  // const filteredArray = R.filter(isEven, numbersArray);
+
+  const filteredArray = numbersArray.filter(number => number % 2 === 0)
+  filteredArray.forEach(element => {
+    let li = document.createElement("li")
+    li.append(element);
+    ul.append(li);
+  });
 });
